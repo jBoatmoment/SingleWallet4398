63ded96e3ab20699eeb84a9bf9ec2c93bfb0a6fd
bfm31 <bfm31@txstate.edu>
2026-02-17 11:31:45 -0600

    Setup commit

---
+++ src/main/java/com/GroupSix/SingleWallet/SingleWalletApplication.java
@@ -0,0 +1,13 @@
+package com.GroupSix.SingleWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleWalletApplication.class, args);
+	}
+
+}
